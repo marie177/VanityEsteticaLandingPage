b7cdc5661b658f4eda3fd39ceb7a9ce5f3a05321
marie177 <gottigmarianella@hotmail.com>
2023-09-03 19:12:34 -0300

    .

--- src/App.js
+++ src/App.js
@@ -2,26 +2,28 @@ import './Styles/App.css';
 import './Styles/AppMobile.css'
 import './Styles/AppTablet.css'
 import Home from "./Components/Home";
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import Treatment from './Components/Treatments/Treatment';
 
 function App() {
-  return (   
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={ <Home />} />
-        <Route path="treatments" element={ <Treatment />} />
-        <Route path="treatments/:treatmentCategory" element={ <Treatment />} />
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="treatments" element={<Treatment />} />
+          <Route path="treatments/:treatmentCategory" element={<Treatment />} />
 
-        <Route path="/VanityEsteticaLandingPage" element={ <Home />} />
-        <Route path="/VanityEsteticaLandingPage/treatments" element={ <Treatment /> } />
-        <Route path="/VanityEsteticaLandingPage/treatments/:treatmentCategory" element={ <Treatment />} />
-      </Routes>
-      <Footer />
-    </div>
+          <Route path="/VanityEsteticaLandingPage" element={<Home />} />
+          <Route path="/VanityEsteticaLandingPage/treatments" element={<Treatment />} />
+          <Route path="/VanityEsteticaLandingPage/treatments/:treatmentCategory" element={<Treatment />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
