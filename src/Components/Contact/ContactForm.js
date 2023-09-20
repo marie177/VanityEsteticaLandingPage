964f18537fcd8ff15c12e8a7cc28d127ffab51c3
marie177 <gottigmarianella@hotmail.com>
2023-09-20 17:32:22 -0300

    add mejoras form consulta

--- src/Components/Contact/ContactForm.js
+++ src/Components/Contact/ContactForm.js
@@ -1,62 +1,103 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import TratamientosCorporales from '../../json/tratamientoscorporales.json'
+import TratamientosFaciales from '../../json/tratamientosfaciales.json'
+import TratamientosCosmetologicos from '../../json/tratamientoscosmetologicos.json'
+import TratamientosBellezaDeOjos from '../../json/tratamientosbellezadeojos.json'
+import TratamientosMasajes from '../../json/tratamientosmasajes.json'
 
 const ContactForm = () => {
     const form = useRef();
 
     const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs.sendForm('service_m0zssnh', 'template_e2i82fa', form.current, 'OllQgh0IDBjivMG8v')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
+        e.preventDefault();
+
+        emailjs.sendForm('service_m0zssnh', 'template_e2i82fa', form.current, 'OllQgh0IDBjivMG8v')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
     };
-    
+
+    const [tratamientosArray, setTratamientos] = useState(TratamientosCorporales);
+
+    const changeTratamientosArray = (tipoTratamiento) => {
+
+        let tratamientosArray = [];
+
+        switch (tipoTratamiento) {
+            case "Corporales":
+                tratamientosArray = TratamientosCorporales;
+                break;
+            case "Faciales":
+                tratamientosArray = TratamientosFaciales;
+                break;
+            case "Cosmetologicos" || "Cosmetológicos":
+                tratamientosArray = TratamientosCosmetologicos;
+                break;
+            case "Belleza_de_ojos":
+                tratamientosArray = TratamientosBellezaDeOjos;
+                break;
+            case "Masajes":
+                tratamientosArray = TratamientosMasajes;
+                break;
+            default:
+                break;
+        }
+
+        setTratamientos(tratamientosArray);
+    }
+
     return (
         <div className='contact-form-container'>
             <form className='contact-form' ref={form} onSubmit={sendEmail}>
                 <div className='row'>
                     <div className='column'>
                         <label className='label-form'>Nombre</label>
-                        <input type='text' className='input-form' name="user_name"/>
+                        <input type='text' className='input-form' name="user_name" />
                     </div>
                     <div className='column'>
                         <label className='label-form'>Apellido</label>
-                        <input type='text' className='input-form' name="user_lastname"/>
+                        <input type='text' className='input-form' name="user_lastname" />
                     </div>
                 </div>
 
                 <div className='row'>
-                <div className='column'>
+                    <div className='column'>
                         <label className='label-form'>Telefono</label>
-                        <input type='text' className='input-form' name="user_telephone"/>
+                        <input type='text' className='input-form' name="user_telephone" />
                     </div>
                     <div className='column'>
                         <label className='label-form'>Email</label>
-                        <input type='email' className='input-form' name="user_email"/>
+                        <input type='email' className='input-form' name="user_email" />
                     </div>
                 </div>
 
                 <div className='row'>
                     <div className='column'>
                         <label className='label-form'>Tipo de Tratamiento</label>
-                        <select name="select-tipo-tratamiento" className='input-form select'>
-                            <option>Tratamientos Corporales</option>
-                            <option>Tratamientos Faciales</option>
-                            <option>Tratamientos Cosmetológicos</option>
-                            <option>Belleza de Ojos</option>
-                            <option>Masajes</option>
+                        <select name="select-tipo-tratamiento" className='input-form select' onChange={e => changeTratamientosArray(e.target.value)}>
+                            <option value="Corporales">Tratamientos Corporales</option>
+                            <option value="Faciales">Tratamientos Faciales</option>
+                            <option value="Cosmetologicos">Tratamientos Cosmetológicos</option>
+                            <option value="Belleza_de_ojos">Belleza de Ojos</option>
+                            <option value="Masajes">Masajes</option>
                         </select>
                     </div>
+                </div>
+
+                <div className='row'>
                     <div className='column'>
                         <label className='label-form'>Tratamiento</label>
                         <select name="select-nombre-tratamiento" className='input-form select'>
-                            <option>VENUS LEGACY</option>
-                            <option>VELA SLIM + BODY GO</option>
-                            <option>LIPO LASER + VELA SLIM</option>
+                            {
+                                tratamientosArray.map(tratamiento => {
+                                    return (
+                                        <option>{tratamiento.title}</option>
+                                    )
+                                })
+                            }
                         </select>
                     </div>
                 </div>
@@ -64,14 +105,14 @@ const ContactForm = () => {
                 <div className='row'>
                     <div className='column'>
                         <label className='label-form'>Consulta</label>
-                        <textarea name="message" className='input-form h-100px' placeholder="Quiero recibir más información sobre el tratamiento..."/>
+                        <textarea name="message" className='input-form h-100px' placeholder="Quiero recibir más información sobre el tratamiento..." />
                     </div>
                 </div>
 
                 <div className='row'>
                     <div className='column'>
                         <button className='btn-submit-form' type='submit'>ENVIAR CONSULTA
-                        <span className='contact-btn-submit-icon'></span></button>
+                            <span className='contact-btn-submit-icon'></span></button>
                     </div>
                 </div>
             </form>
